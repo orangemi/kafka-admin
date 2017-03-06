1d54a1577787b76451405b0a3bf79051992223a6
Orange Mi <orangemiwj@gmail.com>
2017-03-06 14:03:09 +0800

    fix docker/configure.sh file

--- docker/bin/configure.sh
+++ docker/bin/configure.sh
@@ -6,7 +6,7 @@ do
     config_name=`echo "$VAR" | sed -r "s/KAFKA_ADMIN_(.*)=.*/\1/g" | tr '[:upper:]' '[:lower:]' | tr _ .`
     env_var=`echo "$VAR" | sed -r "s/(.*)=.*/\1/g"`
     if egrep -q "(^|^#)$config_name=" $KAFKA_ADMIN_HOME/config/config.properties; then
-      sed -r -i "s@(^|^#)($config_name)=(.*)@\2=${!env_var}@g" $KAFKA_ADMIN_HOME/etc/worker.properties #note that no config values may contain an '@' char
+      sed -r -i "s@(^|^#)($config_name)=(.*)@\2=${!env_var}@g" $KAFKA_ADMIN_HOME/config/config.properties #note that no config values may contain an '@' char
     else
       echo "$config_name=${!env_var}" >> $KAFKA_ADMIN_HOME/config/config.properties
     fi
